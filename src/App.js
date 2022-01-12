ba7ef0f8099b685d0fa0705bb067d2de40958c14
Michael Phelps <michaeljphelps1978@gmail.com>
2022-01-12 18:39:11 +0000

    First Commit - Scaffolded, and http request sent

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch(`https://swapi.dev/api/films`);
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && (
+          <p>
+            Nothing to Show here - Try Clicking the button{" "}
+            <span role="img" aria-label="up_arrow" alt="up">
+              ☝
+            </span>
+          </p>
+        )}
+        {isLoading && <p>Loading... </p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
